b76fff750a0849646dea9661a67519668621450e
Aaron Mathews <84430913+TheB4tMite@users.noreply.github.com>
2024-05-13 17:31:33 +0530

    Add files via upload

---
+++ src/pwn_engine.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import requests
+
+def search_google(query, api_key, cx):
+    url = f"https://www.googleapis.com/customsearch/v1?key={api_key}&cx={cx}&q={query}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('items', [])
+    else:
+        print("ERROR: ", response.status_code)
+        return None
+
+
+api_key = "API_KEY HERE"
+cx = "902e6399d12e5411c"
+query = ""
+
+while True:
+    query = input("[SEARCH QUERY]\n>> ")
+    if (query == "!quit"):
+        break
+    results = search_google(query, api_key, cx)
+
+    if results:
+        print("\n[RESULTS]")
+        for i, item in enumerate(results, 1):
+            print(f"{i}. {item['title']}: {item['link']}")
+    else:
+        print("NO RESULTS FOUND.")
+    print("")
